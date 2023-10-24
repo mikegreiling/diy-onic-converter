dfb487182445da648381e8bba4486a322238b1bd
Mike Greiling <mike@pixelcog.com>
2023-10-24 13:10:52 -0500

    Initial naive implementation

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -3,10 +3,36 @@
  */
 const diyOnicConverter = (textContentContainerSelector) => {
   const container = document.querySelector(textContentContainerSelector);
-  console.log("Performing bionic reading conversion on:", container);
+  console.log('Performing bionic reading conversion on:', container);
 
-  /* Etc. etc. etc. your code etc. */
+  // Get all <p> tags in the container and process them.
+  const paragraphs = container.querySelectorAll('p');
+  paragraphs.forEach((paragraph) => naiveProcessNode(paragraph));
 };
+
+/**
+ * Initial naive implementation. Ignores HTML tags and just processes
+ * words from node.textContent
+ */
+const naiveProcessNode = (node) => {
+  const words = node.textContent.split(/\s+/);
+  node.innerHTML = words.map((word) => naiveProcessWord(word)).join(' ');
+};
+
+/**
+ * Takes a string and returns HTML string which highlights the first
+ * three characters
+ */
+const naiveProcessWord = (word, highlightChars = 3) => {
+  const toHighlight = word.slice(0, highlightChars);
+  const theRest = word.slice(highlightChars);
+  return `<span class='bonic-word' style='font-weight: bold;'>${toHighlight}</span>${theRest}`;
+};
+
+// For ease of development, automatically run this on page load
+window.addEventListener('load', () => {
+  diyOnicConverter('body');
+});
 
 // Allow global access so that this can be executed from the console.
 window.diyOnicConverter = diyOnicConverter;
